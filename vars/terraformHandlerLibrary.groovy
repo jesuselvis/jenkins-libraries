9b3b7b533c1bba6a315f0bda66c9946149e4ce32
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-03 17:02:45 -0500

    v0.0.41

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -5,15 +5,34 @@ def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLO
     String access_key = "none"
     String secret_key = "none"
     String awsProviderContent = "\nprovider \"aws\" {\n\t region = \"us-east-1\"\n\taccess_key = \"${access_key}\"\n\tsecret_key = \"${secret_key}\"\n}"
-    sh "cp -f ${backend} ${workdir}/ && chmod -R 777 ${workdir}/"    
-    dir(workdir) {
-        try {
-            // sh "echo '${awsProviderContent}' >> ${backendName}"
-            new File(backendName).append(awsProviderContent)
-        } 
-        catch (Exception e) {
-            error("> createAwsBackend ::: Add AWS Credentials ::: ${e.message}")         
-        }
+    // sh "cp -f ${backend} ${workdir}/"    
+    // dir(workdir) {
+    //     try {
+    //         // sh "echo '${awsProviderContent}' >> ${backendName}"
+    //         // new File(backendName).append(awsProviderContent)
+    //     } 
+    //     catch (Exception e) {
+    //         error("> createAwsBackend ::: Add AWS Credentials ::: ${e.message}")         
+    //     }
+    // }
+    try {
+        // Copiar el archivo backend al directorio de trabajo y cambiar permisos
+        sh "cp -f ${backend} ${workdir}/"
+        
+        // Define la ruta completa del archivo destino
+        String destinationFilePath = "${workdir}/${backendName}"
+        
+        // Ajustar permisos para permitir la escritura (opcional y de acuerdo a las necesidades)
+        sh "chmod 644 ${destinationFilePath}"
+        
+        // Abrir el archivo destino para agregar contenido de AWS
+        new File(destinationFilePath).append(awsProviderContent)
+        
+        println "Contenido de AWS agregado correctamente a ${destinationFilePath}"
+        
+    } catch (Exception e) {
+        // Manejar errores de permisos
+        error("> createAwsBackend ::: Error al agregar credenciales de AWS: ${e.message}")
     }
 }
 
