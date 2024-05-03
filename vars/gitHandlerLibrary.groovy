d42fb3d351be8efe4a8f9afa028ecb43e2f266c8
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-02 20:57:35 -0500

    v0.0.9

--- vars/gitHandlerLibrary.groovy
+++ vars/gitHandlerLibrary.groovy
@@ -1,29 +1,21 @@
 import commons.Constants
 
-class GitHandler {
-    def script
-
-    GitHandler(script) {
-        this.script = script
-    }
-
-    def Checkout(String repository, String source, String branch) {
-        script.dir(source) {
-            script.checkout([$class: 'GitSCM',
+def checkout(String repository, String source, String branch) {
+    dir(source) {
+        checkout([$class: 'GitSCM',
                       branches: [[name: "${branch}"]],
                       doGenerateSubmoduleConfigurations: false,
                       extensions: [],
                       submoduleCfg: [],
                       userRemoteConfigs: [[/*credentialsId: 'git_credentials',*/ url: repository]]
                      ])
-        }
     }
+}
 
-    def Config(Map param = [:]) {
-        def source = param.get("source", '.')
-        def repository = Constants.DEPLOY_PROJECT
-
-        script.dir(source) {
+def config(Map param = [:]) {
+    def source = param.get("source", '.')
+    def repository = Constants.DEPLOY_PROJECT
+    script.dir(source) {
             script.checkout([$class: 'GitSCM',
                       branches: [[name: 'master']],
                       doGenerateSubmoduleConfigurations: false,
@@ -31,6 +23,5 @@ class GitHandler {
                       submoduleCfg: [],
                       userRemoteConfigs: [[credentialsId: 'git_credentials', url: repository]]
                      ])
-        }
     }
 }
