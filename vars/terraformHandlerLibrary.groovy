7285d0c8d63dcdadef747f2cb3a4cc3ceb0029db
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-03 09:20:22 -0500

    v0.0.14

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -1,8 +1,21 @@
 import commons.Constants
 
-def createAwsBackend(String backenDir, String source = ".") {
-    dir(Constants.WORKDIR_TO_DEPLOY) {
-        sh "ls -lS"
+def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {    
+    String backendName = backend.split("/").last()
+    String awsProvider = '''
+        provider "aws" {
+            region  = "us-east-1"
+        }
+        '''
+    sh "cp -Rf ${backend} ${workdir}/ || true"    
+    dir(workdir) {
+        def backendFile = new File(backendName)
+        if (backendFile.exists()) {
+            backendFile << awsProvider
+            println backendFile.text
+        } else {            
+            error("El archivo ${backendName} no existe en el directorio ${workdir}")
+        }
     }
 }
 
