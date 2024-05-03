ebab721a9fe3afcc1480f12f6fc7af10a3c2495a
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-03 16:03:39 -0500

    v0.0.40

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -1,38 +1,17 @@
 import commons.Constants
-import java.nio.file.*
 
-// def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {    
-//     String backendName = backend.split("/").last()      
-//     String awsProvider = '\nprovider "aws" {\n\t region = "us-east-1"\n}'   
-//     sh "cp -f ${backend} ${workdir}/"    
-//     dir(workdir) {
-//         try {
-//             sh " echo '${awsProvider}' >> ${backendName}"
-//             sh "cat ${backendName}"
-//         } 
-//         catch (Exception e) {
-//             error("> createAwsBackend ::: Add AWS Credentials ::: ${e.message}")            
-//         }
-//     }
-// }
-
-def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {
+def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {    
     String backendName = new File(backend).name  
     String awsProviderContent = "provider \"aws\" {\n\t region = \"us-east-1\"\n}\n"
-    println("workdir: ${workdir}, backend: ${backend}")
-    
-    def sourceFile = new File(backend)
-    String destinationPath = Paths.get(workdir, backendName).toString()
-    println("workdir: ${destinationPath}")
-
-    Files.copy(sourceFile.toPath(), Paths.get(destinationPath), StandardCopyOption.REPLACE_EXISTING) 
-
-    // try {           
-    //     Files.copy(sourceFile.toPath(), Paths.get(destinationPath), StandardCopyOption.REPLACE_EXISTING)   
-    // } 
-    // catch (Exception e) {        
-    //     error("> createAwsBackend ::: Error al agregar configuraciones de AWS: ${e.message}")
-    // }
+    sh "cp -f ${backend} ${workdir}/"    
+    dir(workdir) {
+        try {
+            sh "echo '${awsProvider}' >> ${backendName} && cat ${backendName}"
+        } 
+        catch (Exception e) {
+            error("> createAwsBackend ::: Add AWS Credentials ::: ${e.message}")            
+        }
+    }
 }
 
 def initialize() {
