6385fcda1873e1482441bdd77c880ba229ad43a8
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-03 11:43:32 -0500

    v0.0.27

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -1,31 +1,48 @@
 import commons.Constants
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.charset.StandardCharsets
 
-def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {    
+def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {
+    // Extraer el nombre del archivo backend
     String backendName = backend.split("/").last()
+    
+    // Definir la configuración de AWS
     String awsProvider = '''
         provider "aws" {
-            region  = "us-east-1"
+            region = "us-east-1"
         }
-        '''
-    
-    // File backendFile = new File(backend)
-    // File workDir = new File(workdir)
-    
-    // if (!backendFile.exists()) {
-    //     error("El archivo ${backend} no existe.")
-    // }
+    '''
 
-    Files.copy(backend, Paths.get(workdir, backendName))
-
-    File destinationFile = new File(workdir, backendName)
-    
-    destinationFile.withWriterAppend('UTF-8') { writer ->
-        writer.writeLine(awsProvider)
+    // Crear el directorio de trabajo si no existe
+    def workDirPath = Paths.get(workdir)
+    if (!Files.exists(workDirPath)) {
+        Files.createDirectories(workDirPath)
     }
-
-    println destinationFile.text
+    
+    // Copiar el archivo backend al directorio de trabajo
+    def sourcePath = Paths.get(backend)
+    def destinationPath = workDirPath.resolve(backendName)
+    
+    // Copia el archivo backend al directorio de trabajo
+    try {
+        Files.copy(sourcePath, destinationPath)
+    } catch (Exception e) {
+        println "Error copiando el archivo backend: ${e.message}"
+        return
+    }
+    
+    // Añadir la configuración de AWS al archivo backend
+    if (Files.exists(destinationPath)) {
+        try {
+            def existingContent = new String(Files.readAllBytes(destinationPath), StandardCharsets.UTF_8)
+            Files.write(destinationPath, (existingContent + awsProvider).getBytes(StandardCharsets.UTF_8))
+        } catch (Exception e) {
+            println "Error escribiendo en el archivo backend: ${e.message}"
+        }
+    } else {
+        println "El archivo backend ${backendName} no existe en ${workdir}"
+    }
 }
 
 def initialize() {
