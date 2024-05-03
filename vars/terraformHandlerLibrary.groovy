7da54e12abb9f8de9c8fb687bbf39dfef8f7594e
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-03 10:07:13 -0500

    v0.0.22

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -9,7 +9,7 @@ def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLO
         '''
     sh "cp -f ${backend} ${workdir}/ || true"    
     dir(workdir) {
-        sh "echo '${awsProvider}' >> ${backenName}"
+        // sh "echo '${awsProvider}' >> ${backenName}"
         sh "cat ${backenName}"
     }
 }
