b21d389f35d327c5a18ea18a4b6ed194963b793a
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-03 16:10:20 -0500

    v0.0.40

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -2,14 +2,14 @@ import commons.Constants
 
 def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {    
     String backendName = new File(backend).name  
-    String awsProviderContent = "provider \"aws\" {\n\t region = \"us-east-1\"\n}\n"
+    String awsProviderContent = "\nprovider \"aws\" {\n\t region = \"us-east-1\"\n}"
     sh "cp -f ${backend} ${workdir}/"    
     dir(workdir) {
         try {
             sh "echo '${awsProviderContent}' >> ${backendName} && cat ${backendName}"
         } 
         catch (Exception e) {
-            error("> createAwsBackend ::: Add AWS Credentials ::: ${e.message}")            
+            error("> createAwsBackend ::: Add AWS Credentials ::: ${e.message}")         
         }
     }
 }
