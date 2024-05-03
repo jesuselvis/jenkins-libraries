02faf9a58ea66f00db3963e6e2d46acd5c3efee9
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-03 13:09:11 -0500

    v0.0.37

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -1,16 +1,24 @@
 import commons.Constants
 
-def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {    
-    """
-    Creates an AWS backend configuration file.
-    Args:
-      backend (str): Path to the base backend configuration file.
-      workdir (str, optional): Working directory for the operation. Defaults to "WORKDIR_TO_DEPLOY".
-    Returns:
-      None
-    """
+// def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {    
+//     String backendName = backend.split("/").last()      
+//     String awsProvider = '\nprovider "aws" {\n\t region = "us-east-1"\n}'   
+//     sh "cp -f ${backend} ${workdir}/"    
+//     dir(workdir) {
+//         try {
+//             sh " echo '${awsProvider}' >> ${backendName}"
+//             sh "cat ${backendName}"
+//         } 
+//         catch (Exception e) {
+//             error("> createAwsBackend ::: Add AWS Credentials ::: ${e.message}")            
+//         }
+//     }
+// }
+
+def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY) {
   // Extract filename from backend path
   def backendName = new File(backend).name
+  sh "echo '${backendName}'"
   // Combine AWS provider config with newline
   def awsProviderContent = "provider \"aws\" {\n\t region = \"us-east-1\"\n}\n"
   // Use try-with-resource for automatic file closing and error handling
