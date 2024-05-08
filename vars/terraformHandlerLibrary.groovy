5f2bca659e08e69fb8012aaa2430892cb010d83f
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-07 19:52:14 -0500

    v0.0.40

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -1,18 +1,32 @@
 import commons.Constants
 
-def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY, String awsRegion="us-east-1") {    
-    String backendName = new File(backend).name
+// def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY, String awsRegion="us-east-1") {    
+//     String backendName = new File(backend).name
 
-    sh "cp -f ${backend} ${workdir}/"    
+//     sh "cp -f ${backend} ${workdir}/"    
     
-    dir(workdir) {
+//     dir(workdir) {
+//         withCredentials([[
+//             $class: 'AmazonWebServicesCredentialsBinding',
+//             credentialsId: 'aws_account_dev_dataplatform',
+//             accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+//             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+//         ]]) {
+//             sh "echo '\nprovider \"aws\" {\n\tregion = \"${awsRegion}\"\n\taccess_key = \"${AWS_ACCESS_KEY_ID}\"\n\tsecret_key = \"${AWS_SECRET_ACCESS_KEY}\"\n}' >> ${backendName}"
+//         }
+//     }
+// }
+
+def createAwsBackend(String _bucketName, String _bucketKey, String _credentials, String _awsRegion="us-east-1",String _workdir = Constants.WORKDIR_TO_DEPLOY) {
+    dir(_workdir) {
         withCredentials([[
             $class: 'AmazonWebServicesCredentialsBinding',
-            credentialsId: 'aws_account_dev_dataplatform',
+            credentialsId: _credentials,
             accessKeyVariable: 'AWS_ACCESS_KEY_ID',
             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
         ]]) {
-            sh "echo '\nprovider \"aws\" {\n\tregion = \"${awsRegion}\"\n\taccess_key = \"${AWS_ACCESS_KEY_ID}\"\n\tsecret_key = \"${AWS_SECRET_ACCESS_KEY}\"\n}' >> ${backendName}"
+            sh "echo '\nterraform{\n\tbackend \"s3\" {\n\t\tbucket = \"${_bucketName}\"\n\t\tencrypt = true\n\t\tkey = \"${_bucketKey}\"\n\t\tregion = \"${_awsRegion}\"\n\t\taccess_key = \"${AWS_ACCESS_KEY_ID}\"\n\t\tsecret_key = \"${AWS_SECRET_ACCESS_KEY}\"\n\t}\n}\n' > backend.tf"
+            sh "cat backend.tf"
         }
     }
 }
