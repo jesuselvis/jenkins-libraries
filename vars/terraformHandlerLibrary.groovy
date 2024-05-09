0a4d168d371091b0fee931dc5b3fe616f7b899e3
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-08 22:09:29 -0500

    v0.0.41

--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -21,10 +21,10 @@ def initialize(String _workdir = Constants.WORKDIR_TO_DEPLOY) {
 }
 
 def plan(String _workdir = Constants.WORKDIR_TO_DEPLOY) {
-    sh "cd Develop/DataPlatform && ls -l"
+    sh "cd ${WORKDIR}/Develop/DataPlatform && ls -l"
     String rutaArchivoYAML = 'Develop/DataPlatform/env.yml'
     Yaml yaml = new Yaml()
-    def contenidoYAML = new File(rutaArchivoYAML).text
+    def contenidoYAML = new File(WORKDIR+'/'+rutaArchivoYAML).text
     def variables = yaml.load(contenidoYAML)
     def comandoTerraform = 'terraform plan'
     variables.each { clave, valor ->
