6faf65324e3f02b8bd11269d8a2386a5145165a2
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-08 19:16:47 -0500

    v0.0.41

--- src/commons/Constants.groovy
+++ src/commons/Constants.groovy
@@ -4,11 +4,16 @@ package commons
 class Constants {
 	public static String DEPLOY_PROJECT	= '.'
 	public static String REPO_CONFIG	= ''
-	public static String WORKDIR_TO_DEPLOY	= 'workDirToDeploy'
+	public static String WORKDIR_TO_DEPLOY	= 'workdirToDeploy'
     
-    public static Map GIT_PIPELINE_BRANCHES = [ dev 	: 	'dev',
-    										test  	    : 	'test',
-    										prod 		: 	'prod' ]
+    public static Map TF_STATES_S3_BUCKET = [ 	develop 	: 	'devops-iac-dev-jenkins-tfstates',
+    											test  	    : 	'devops-iac-test-jenkins-tfstates',
+    											production	: 	'devops-iac-prod-jenkins-tfstates']
+	
+	public static Map TF_STATES_CREDENTIALS_ID = [	develop 	: 	'AWS_TFSTATES__BACKEND_ACCOUNT',
+    												test  	    : 	'',
+    												production	: 	'' ]
+	
 
 	public static Map GIT_JENKINS_PIPELINES	= [ dataPlatform	: 	'https://github.com/victorjhqampier/jenkins-pipelines.git' ]
 }
--- vars/terraformHandlerLibrary.groovy
+++ vars/terraformHandlerLibrary.groovy
@@ -1,32 +1,14 @@
 import commons.Constants
 
-// def createAwsBackend(String backend, String workdir = Constants.WORKDIR_TO_DEPLOY, String awsRegion="us-east-1") {    
-//     String backendName = new File(backend).name
-
-//     sh "cp -f ${backend} ${workdir}/"    
-    
-//     dir(workdir) {
-//         withCredentials([[
-//             $class: 'AmazonWebServicesCredentialsBinding',
-//             credentialsId: 'aws_account_dev_dataplatform',
-//             accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-//             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-//         ]]) {
-//             sh "echo '\nprovider \"aws\" {\n\tregion = \"${awsRegion}\"\n\taccess_key = \"${AWS_ACCESS_KEY_ID}\"\n\tsecret_key = \"${AWS_SECRET_ACCESS_KEY}\"\n}' >> ${backendName}"
-//         }
-//     }
-// }
-
-def createAwsBackend(String _bucketName, String _bucketKey, String _credentials, String _awsRegion="us-east-1",String _workdir = Constants.WORKDIR_TO_DEPLOY) {
+def createAwsBackend(String _bucketKey, String _environment, String _awsRegion="us-east-1",String _workdir = Constants.WORKDIR_TO_DEPLOY) {
     dir(_workdir) {
         withCredentials([[
             $class: 'AmazonWebServicesCredentialsBinding',
-            credentialsId: _credentials,
+            credentialsId: Constants.TF_STATES_CREDENTIALS_ID[_environment],
             accessKeyVariable: 'AWS_ACCESS_KEY_ID',
             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
         ]]) {
-            sh "echo '\nterraform{\n\tbackend \"s3\" {\n\t\tbucket = \"${_bucketName}\"\n\t\tencrypt = true\n\t\tkey = \"${_bucketKey}\"\n\t\tregion = \"${_awsRegion}\"\n\t\taccess_key = \"${AWS_ACCESS_KEY_ID}\"\n\t\tsecret_key = \"${AWS_SECRET_ACCESS_KEY}\"\n\t}\n}\n' > backend.tf"
-            sh "cat backend.tf"
+            sh "echo '\nterraform{\n\tbackend \"s3\" {\n\t\tbucket = \"${Constants.TF_STATES_S3_BUCKET[_environment]}\"\n\t\tencrypt = true\n\t\tkey = \"${_bucketKey}\"\n\t\tregion = \"${_awsRegion}\"\n\t\taccess_key = \"${AWS_ACCESS_KEY_ID}\"\n\t\tsecret_key = \"${AWS_SECRET_ACCESS_KEY}\"\n\t}\n}\n' > backend.tf"            
         }
     }
 }
